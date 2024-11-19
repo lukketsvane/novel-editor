53533373e35ebbbd5bc8a90d313287f434070f04
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-11-19 01:14:53 +0000

    hy

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['novel'],
-  swcMinify: true,
   images: {
     domains: ['i.ibb.co', 'github.com', 'raw.githubusercontent.com'],
   },
   experimental: {
-    appDir: true,
-    serverActions: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
   },
 };
 
